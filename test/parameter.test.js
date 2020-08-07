44d473ab142ea35d4dbf227350f702d3fdf89b49
Restyled.io <commits@restyled.io>
2020-08-07 17:11:39 +0000

    Restyled by clang-format

--- test/parameter.test.js
+++ test/parameter.test.js
@@ -1,12 +1,11 @@
-var test = require('tape'),
-	convert = require('../');
+var test = require('tape'), convert = require('../');
 
 test('converting a minimal OpenAPI 3.0 parameter', function(assert) {
-	var schema, result, expected;
+  var schema, result, expected;
 
-	assert.plan(1);
+  assert.plan(1);
 
-	schema = {
+  schema = {
 		name: 'parameter name',
 		in: 'cookie',
 		schema: {
@@ -15,22 +14,22 @@ test('converting a minimal OpenAPI 3.0 parameter', function(assert) {
 		}
 	};
 
-	result = convert.fromParameter(schema);
+  result = convert.fromParameter(schema);
 
-	expected = {
-		$schema: 'http://json-schema.org/draft-04/schema#',
-		type: ['string', 'null'],
-	};
+  expected = {
+    $schema : 'http://json-schema.org/draft-04/schema#',
+    type : [ 'string', 'null' ],
+  };
 
-	assert.deepEqual(result, expected, 'OpenAPI 3.0 parameter converted');
+  assert.deepEqual(result, expected, 'OpenAPI 3.0 parameter converted');
 });
 
 test('converting an extensive OpenAPI 3.0 parameter', function(assert) {
-	var schema, result, expected;
+  var schema, result, expected;
 
-	assert.plan(1);
+  assert.plan(1);
 
-	schema = {
+  schema = {
 		name: 'parameter name',
 		in: 'cookie',
 		schema: {
@@ -46,22 +45,22 @@ test('converting an extensive OpenAPI 3.0 parameter', function(assert) {
 		example: 'parameter example'
 	};
 
-	result = convert.fromParameter(schema);
+  result = convert.fromParameter(schema);
 
-	expected = {
-		$schema: 'http://json-schema.org/draft-04/schema#',
-		type: ['string', 'null'],
-	};
+  expected = {
+    $schema : 'http://json-schema.org/draft-04/schema#',
+    type : [ 'string', 'null' ],
+  };
 
-	assert.deepEqual(result, expected, 'OpenAPI 3.0 parameter converted');
+  assert.deepEqual(result, expected, 'OpenAPI 3.0 parameter converted');
 });
 
 test('converting a OpenAPI 3.0 parameter with MIME schemas', function(assert) {
-	var schema, result, expected;
+  var schema, result, expected;
 
-	assert.plan(1);
+  assert.plan(1);
 
-	schema = {
+  schema = {
 		name: 'parameter name',
 		in: 'cookie',
 		content: {
@@ -80,28 +79,29 @@ test('converting a OpenAPI 3.0 parameter with MIME schemas', function(assert) {
 		},
 	};
 
-	result = convert.fromParameter(schema);
+  result = convert.fromParameter(schema);
 
-	expected = {
-		'application/javascript': {
-			$schema: 'http://json-schema.org/draft-04/schema#',
-			type: ['string', 'null'],
-		},
-		'text/css': {
-			$schema: 'http://json-schema.org/draft-04/schema#',
-			type: ['string', 'null'],
-		},
-	};
+  expected = {
+    'application/javascript' : {
+      $schema : 'http://json-schema.org/draft-04/schema#',
+      type : [ 'string', 'null' ],
+    },
+    'text/css' : {
+      $schema : 'http://json-schema.org/draft-04/schema#',
+      type : [ 'string', 'null' ],
+    },
+  };
 
-	assert.deepEqual(result, expected, 'parameter MIMEs schemas converted');
+  assert.deepEqual(result, expected, 'parameter MIMEs schemas converted');
 });
 
-test('converting a OpenAPI 3.0 parameter with MIMEs without a schema', function(assert) {
-	var schema, result, expected;
+test('converting a OpenAPI 3.0 parameter with MIMEs without a schema',
+     function(assert) {
+       var schema, result, expected;
 
-	assert.plan(1);
+       assert.plan(1);
 
-	schema = {
+       schema = {
 		name: 'parameter name',
 		in: 'cookie',
 		content: {
@@ -115,27 +115,27 @@ test('converting a OpenAPI 3.0 parameter with MIMEs without a schema', function(
 		},
 	};
 
-	result = convert.fromParameter(schema);
+       result = convert.fromParameter(schema);
 
-	expected = {
-		'application/javascript': {
-			$schema: 'http://json-schema.org/draft-04/schema#',
-			type: ['string', 'null'],
-		},
-		'text/css': {
-			$schema: 'http://json-schema.org/draft-04/schema#',
-		},
-	};
+       expected = {
+         'application/javascript' : {
+           $schema : 'http://json-schema.org/draft-04/schema#',
+           type : [ 'string', 'null' ],
+         },
+         'text/css' : {
+           $schema : 'http://json-schema.org/draft-04/schema#',
+         },
+       };
 
-	assert.deepEqual(result, expected, 'MIME without a schema converted');
-});
+       assert.deepEqual(result, expected, 'MIME without a schema converted');
+     });
 
 test('using a OpenAPI 3.0 parameter description', function(assert) {
-	var schema, result, expected;
+  var schema, result, expected;
 
-	assert.plan(1);
+  assert.plan(1);
 
-	schema = {
+  schema = {
 		name: 'parameter name',
 		in: 'cookie',
 		description: 'parameter description',
@@ -144,48 +144,46 @@ test('using a OpenAPI 3.0 parameter description', function(assert) {
 		}
 	};
 
-	result = convert.fromParameter(schema);
+  result = convert.fromParameter(schema);
 
-	expected = {
-		$schema: 'http://json-schema.org/draft-04/schema#',
-		description: 'parameter description'
-	};
+  expected = {
+    $schema : 'http://json-schema.org/draft-04/schema#',
+    description : 'parameter description'
+  };
 
-	assert.deepEqual(result, expected, 'parameter description used');
+  assert.deepEqual(result, expected, 'parameter description used');
 });
 
 test('throwing on OpenAPI 3.0 parameters without schemas', function(assert) {
-	var schema;
+  var schema;
 
-	assert.plan(1);
+  assert.plan(1);
 
-	schema = {
+  schema = {
 		name: 'parameter name',
 		in: 'cookie',
 	};
 
-	assert.throws(function() {
-		convert.fromParameter(schema);
-	}, /InvalidInputError/);
+  assert.throws(function() { convert.fromParameter(schema); },
+                /InvalidInputError/);
 });
 
-test('doesnt throw for parameters without schemas with stricMode disabled', function(assert) {
-	var schema, result, expected;
+test('doesnt throw for parameters without schemas with stricMode disabled',
+     function(assert) {
+       var schema, result, expected;
 
-	assert.plan(1);
+       assert.plan(1);
 
-	schema = {
+       schema = {
 		name: 'parameter name',
 		in: 'cookie',
 	};
 
-	result = convert.fromParameter(schema, {
-		strictMode: false
-	});
+       result = convert.fromParameter(schema, {strictMode : false});
 
-	expected = {
-		$schema: 'http://json-schema.org/draft-04/schema#',
-	};
+       expected = {
+         $schema : 'http://json-schema.org/draft-04/schema#',
+       };
 
-	assert.deepEqual(result, expected, 'parameter description used');
-});
+       assert.deepEqual(result, expected, 'parameter description used');
+     });
